eeebe02162502f431e8c48cdf6c28db6e1ffa7e9
Ali Sadeqinia <asadeqinia@gmail.com>
2022-03-09 07:57:51 -0800

    first before push

---
+++ Cards/sliding-cards1/js/swiper-initializer.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        slidesPerGroup: 2,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+        slidesPerGroup: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+      },
+    },
+});
---
+++ README.md
@@ -0,0 +1,27 @@
+# Web elements
+> Here is some of my exersice in web elements design.
+
+## List of elements (updating)
+
+### Cards
+
+- contact card 1
+
+![alt text](Cards/contact-card1/assets/contact-card1.png)
+![alt text](Cards/contact-card1/assets/contact-card2.png)
+
+- sliding cards 1
+
+![alt text](Cards/sliding-cards1/images/card-slider.png)
+
+### Login pages
+
+- Responsive Login and Register form with flipper
+
+![alt text](login-page/Responsive-Login-Register-form-with-flipper/assets/view.gif)
+
+### Navbars- Sidebars
+
+- Responsive navbar 1
+
+![alt text](navbar-sidebar/Responsive-navbar1/assets/view.gif)
